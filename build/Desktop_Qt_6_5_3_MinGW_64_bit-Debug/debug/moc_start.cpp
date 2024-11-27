0296d7aec3e60639adc5b2a1a433ea24c9e88f04
blackdover <blackdover@qq.com>
2024-11-27 10:16:05 +0800

    更新

--- build/Desktop_Qt_6_5_3_MinGW_64_bit-Debug/debug/moc_start.cpp
+++ build/Desktop_Qt_6_5_3_MinGW_64_bit-Debug/debug/moc_start.cpp
@@ -41,15 +41,17 @@ static constexpr auto qt_meta_stringdata_CLASSstartENDCLASS = QtMocHelpers::stri
     "start",
     "on_startbutton_clicked",
     "",
+    "on_rankbutton_clicked",
     "on_connectusbutton_clicked"
 );
 #else  // !QT_MOC_HAS_STRING_DATA
 struct qt_meta_stringdata_CLASSstartENDCLASS_t {
-    uint offsetsAndSizes[8];
+    uint offsetsAndSizes[10];
     char stringdata0[6];
     char stringdata1[23];
     char stringdata2[1];
-    char stringdata3[27];
+    char stringdata3[22];
+    char stringdata4[27];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_CLASSstartENDCLASS_t::offsetsAndSizes) + ofs), len 
@@ -58,11 +60,13 @@ Q_CONSTINIT static const qt_meta_stringdata_CLASSstartENDCLASS_t qt_meta_stringd
         QT_MOC_LITERAL(0, 5),  // "start"
         QT_MOC_LITERAL(6, 22),  // "on_startbutton_clicked"
         QT_MOC_LITERAL(29, 0),  // ""
-        QT_MOC_LITERAL(30, 26)   // "on_connectusbutton_clicked"
+        QT_MOC_LITERAL(30, 21),  // "on_rankbutton_clicked"
+        QT_MOC_LITERAL(52, 26)   // "on_connectusbutton_clicked"
     },
     "start",
     "on_startbutton_clicked",
     "",
+    "on_rankbutton_clicked",
     "on_connectusbutton_clicked"
 };
 #undef QT_MOC_LITERAL
@@ -75,7 +79,7 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSstartENDCLASS[] = {
       11,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -83,10 +87,12 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSstartENDCLASS[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   26,    2, 0x08,    1 /* Private */,
-       3,    0,   27,    2, 0x08,    2 /* Private */,
+       1,    0,   32,    2, 0x08,    1 /* Private */,
+       3,    0,   33,    2, 0x08,    2 /* Private */,
+       4,    0,   34,    2, 0x08,    3 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
 
@@ -104,6 +110,8 @@ Q_CONSTINIT const QMetaObject start::staticMetaObject = { {
         QtPrivate::TypeAndForceComplete<start, std::true_type>,
         // method 'on_startbutton_clicked'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_rankbutton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'on_connectusbutton_clicked'
         QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
@@ -117,7 +125,8 @@ void start::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void 
         (void)_t;
         switch (_id) {
         case 0: _t->on_startbutton_clicked(); break;
-        case 1: _t->on_connectusbutton_clicked(); break;
+        case 1: _t->on_rankbutton_clicked(); break;
+        case 2: _t->on_connectusbutton_clicked(); break;
         default: ;
         }
     }
@@ -143,13 +152,13 @@ int start::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 3)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 2;
+        _id -= 3;
     }
     return _id;
 }
