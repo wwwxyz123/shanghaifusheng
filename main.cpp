e1d47240e19fb2d2c88fd851b0a30dcc5dc3f86b
blackdover <blackdover@qq.com>
2024-11-27 22:51:13 +0800

    优化

--- main.cpp
+++ main.cpp
@@ -14,29 +14,5 @@ int main(int argc, char *argv[])
     w.show();
     uitest w2;
     w2.show();
-
-
-    QString filename = ":/res/items.txt";  // 使用相对路径（假设文件存储在资源文件中）
-
-    printFileContents(filename);
-
-
-
     return a.exec();
-}
-
-void printFileContents(const QString& filename) {
-    std::ifstream file(filename.toStdString());  // 打开文件
-    if (!file.is_open()) {  // 文件打开失败
-        std::cerr << "Failed to open file: " << filename.toStdString() << std::endl;
-        return;
-    }
-
-    std::string line;
-    // 逐行读取文件并打印
-    while (std::getline(file, line)) {
-        std::cout << line << std::endl;  // 打印每一行内容
-    }
-
-    file.close();  // 关闭文件
 }
