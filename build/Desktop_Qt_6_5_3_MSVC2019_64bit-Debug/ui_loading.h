5a83a4a982070f6494aadf4a0929aef67b08de35
blackdover <blackdover@qq.com>
2024-12-03 11:44:43 +0800

    忽略文件夹build

--- build/Desktop_Qt_6_5_3_MSVC2019_64bit-Debug/ui_loading.h
+++ build/Desktop_Qt_6_5_3_MSVC2019_64bit-Debug/ui_loading.h
@@ -12,6 +12,7 @@
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QWidget>
 
@@ -23,11 +24,14 @@ public:
     QLabel *label;
     QPushButton *begin;
     QLabel *label_2;
+    QLabel *label_3;
+    QLineEdit *lineEdit;
 
     void setupUi(QWidget *loading)
     {
         if (loading->objectName().isEmpty())
             loading->setObjectName("loading");
+<<<<<<< HEAD
 <<<<<<< HEAD
         loading->resize(410, 583);
         label = new QLabel(loading);
@@ -39,6 +43,12 @@ public:
         label->setObjectName("label");
         label->setGeometry(QRect(10, 100, 391, 361));
 >>>>>>> parent of dcd2381 (优化)
+=======
+        loading->resize(638, 578);
+        label = new QLabel(loading);
+        label->setObjectName("label");
+        label->setGeometry(QRect(10, 50, 601, 421));
+>>>>>>> parent of 4caa02a (优化)
         QFont font;
         font.setPointSize(12);
         label->setFont(font);
@@ -46,23 +56,38 @@ public:
         begin = new QPushButton(loading);
         begin->setObjectName("begin");
 <<<<<<< HEAD
+<<<<<<< HEAD
         begin->setGeometry(QRect(120, 500, 131, 51));
 =======
         begin->setGeometry(QRect(130, 500, 131, 51));
 >>>>>>> parent of dcd2381 (优化)
+=======
+        begin->setGeometry(QRect(470, 510, 131, 51));
+>>>>>>> parent of 4caa02a (优化)
         QFont font1;
         font1.setPointSize(14);
         begin->setFont(font1);
         label_2 = new QLabel(loading);
         label_2->setObjectName("label_2");
 <<<<<<< HEAD
+<<<<<<< HEAD
         label_2->setGeometry(QRect(0, -10, 381, 71));
 =======
         label_2->setGeometry(QRect(20, 10, 381, 71));
 >>>>>>> parent of dcd2381 (优化)
+=======
+        label_2->setGeometry(QRect(130, 0, 381, 71));
+>>>>>>> parent of 4caa02a (优化)
         QFont font2;
         font2.setPointSize(18);
         label_2->setFont(font2);
+        label_3 = new QLabel(loading);
+        label_3->setObjectName("label_3");
+        label_3->setGeometry(QRect(20, 490, 151, 71));
+        label_3->setFont(font);
+        lineEdit = new QLineEdit(loading);
+        lineEdit->setObjectName("lineEdit");
+        lineEdit->setGeometry(QRect(192, 515, 211, 41));
 
         retranslateUi(loading);
 
@@ -77,6 +102,7 @@ public:
                         "\214\346\202\250\347\247\237\347\232\204\346\210\277\345\255\220\345\217\252\350\203\275\346\224\276100\344\273\266\344\270\234\350\245\277\357\274\214\346\202\250\345\217\257\344\273\245\345\234\250\347\247\237\346\210\277\344\270\255\345\277\203\347\247\237\346\233\264\345\244\247\347\232\204\346\210\277\345\255\220\343\200\202\345\271\266\344\270\224\350\246\201\346\263\250\346\204\217\351\232\217\346\234\272\344\272\213\344\273\266\345\257\271\346\202\250\350\272\253\345\277\203\345\201\245\345\272\267\345\270\246\346\235\245\347\232\204\345\275\261\345\223\215\343\200\202</p><p>        \351\202\243\344\271\210\357\274\214\345\246\202\346\236\234\346\202\250\345\207\206\345\244\207\345\245\275\344\272\206\357\274\214\345\260\261\350\277\233\345\205\245\346\270\270\346\210\217\345\220\247\357\274\201</p></body></html>", nullptr));
         begin->setText(QCoreApplication::translate("loading", "\345\207\272\345\217\221", nullptr));
         label_2->setText(QCoreApplication::translate("loading", "<html><head/><body><p align=\"center\"><span style=\" font-weight:700;\">\344\270\212\346\265\267\346\265\256\347\224\237\350\256\260\347\232\204\346\225\205\344\272\213</span></p></body></html>", nullptr));
+        label_3->setText(QCoreApplication::translate("loading", "\350\257\267\350\276\223\345\205\245\346\202\250\347\232\204\345\244\247\345\220\215\357\274\232", nullptr));
     } // retranslateUi
 
 };
