3e0cce169d94251f1e2837f38e73c3831cc1d058
blackdover <blackdover@qq.com>
2024-12-04 21:00:38 +0800

    更改图标和描述文字

--- connectus.cpp
+++ connectus.cpp
@@ -13,6 +13,8 @@ connectUs::connectUs(QWidget *parent)
     ui->github->setText("<a href='https://github.com/smu-cs-cpp-project-team/shanghaifusheng'>点击跳转</a>");
     // ui->github->setTextInteractionFlags(Qt::TextBrowserInteraction); // 允许点击
     ui->github->setOpenExternalLinks(true); // 点击时自动打开外部链接
+    setWindowTitle("联系我们");
+    setWindowIcon(QIcon(":/res/icon.png"));
 }
 
 connectUs::~connectUs()
