fc2a61744c679179e64aae5892fa4751ff30f519
blackdover <blackdover@qq.com>
2024-12-04 17:22:01 +0800

    游戏结束页面

--- uitest.h
+++ uitest.h
@@ -2,7 +2,8 @@
 #define UITEST_H
 
 #include <QWidget>
-
+#include<qlabel.h>
+#include"mainwindow.h"
 namespace Ui {
 class uitest;
 }
@@ -12,12 +13,27 @@ class uitest : public QWidget
     Q_OBJECT
 
 public:
-    explicit uitest(QWidget *parent = nullptr);
+    explicit uitest(MainWindow *main,QWidget *parent = nullptr);
     ~uitest();
     void updateValue();
-    int stepValue;
+
+    void setupAnimation(QLabel* label, int& currentValue, int targetValue);
+private slots:
+    void on_pushButton_clicked();
+
+    void on_torank_clicked();
+
+    void on_newgame_clicked();
+
+    void on_leave_clicked();
+
 private:
     Ui::uitest *ui;
+    MainWindow *mainwindow;
+
+    long long currentValue;
+    long long targetValue;
+    long long stepValue;
 };
 
 #endif // UITEST_H
